1b4632ea900e6657a4d2afc6528112675736594c
tgutierrez <tgutierrez@saepc78.ssi-schaefer.local>
2019-04-29 13:34:49 +0200

    example factory simple

---
+++ src/main/Principal.java
@@ -0,0 +1,24 @@
+package main;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		JugadorFactory jf = new JugadorFactory();
+		Jugador j = jf.crearJugador();
+	}
+	
+	static class Jugador{
+		public Jugador() {
+			System.out.println("creando objeto que requere de una " +
+					" dificil configuracion ");			
+		}
+	}
+	
+	static class JugadorFactory{
+		public Jugador crearJugador() {
+			System.out.println("creando objeto que requiere de varias preparaciones");
+			return new Jugador();
+		}
+	}
+
+}
